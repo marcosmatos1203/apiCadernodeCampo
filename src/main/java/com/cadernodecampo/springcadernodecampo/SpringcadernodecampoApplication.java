9cdc4bd053aad7acaf8e739afe13b761175c8f84
marcosmatos1203 <marcoscaiobel@gmail.com>
2021-09-26 15:12:35 -0300

    iniciando

---
+++ src/main/java/com/cadernodecampo/springcadernodecampo/SpringcadernodecampoApplication.java
@@ -0,0 +1,13 @@
+package com.cadernodecampo.springcadernodecampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcadernodecampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcadernodecampoApplication.class, args);
+	}
+
+}
