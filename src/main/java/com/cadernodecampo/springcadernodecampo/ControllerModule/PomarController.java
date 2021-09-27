c47747e95d4f7150a7d3a67d536d838e9b4d4e26
marcosmatos1203 <marcoscaiobel@gmail.com>
2021-09-27 16:21:04 -0300

    put e path para pomar

--- src/main/java/com/cadernodecampo/springcadernodecampo/ControllerModule/PomarController.java
+++ src/main/java/com/cadernodecampo/springcadernodecampo/ControllerModule/PomarController.java
@@ -1,13 +1,21 @@
 package com.cadernodecampo.springcadernodecampo.ControllerModule;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.cadernodecampo.springcadernodecampo.DTO.PomarDTO;
 import com.cadernodecampo.springcadernodecampo.DominioModel.Pomar;
 import com.cadernodecampo.springcadernodecampo.ServiceModel.PomarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +24,29 @@ public class PomarController {
 
     @Autowired
     private PomarService service;
-    
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Pomar> findById(@PathVariable Integer id){
+    public ResponseEntity<Pomar> findById(@PathVariable Integer id) {
         Pomar obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PomarDTO>> findAll(
+            @RequestParam(value = "produtor", defaultValue = "0") Integer idprod) {
+        List<Pomar> listaPomar = service.findAll(idprod);
+        List<PomarDTO> listaPomarDTO = listaPomar.stream().map(obj -> new PomarDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listaPomarDTO);
+    }
+
+    @PutMapping(value = "/id")
+    public ResponseEntity<Pomar> update(@PathVariable Integer id, @RequestBody Pomar obj) {
+        Pomar newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+    @PatchMapping(value = "/id")
+    public ResponseEntity<Pomar> updatePatch(@PathVariable Integer id, @RequestBody Pomar obj) {
+        Pomar newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
     }
 }
--- src/main/java/com/cadernodecampo/springcadernodecampo/ServiceModel/PomarService.java
+++ src/main/java/com/cadernodecampo/springcadernodecampo/ServiceModel/PomarService.java
@@ -1,5 +1,6 @@
 package com.cadernodecampo.springcadernodecampo.ServiceModel;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.cadernodecampo.springcadernodecampo.DominioModel.Pomar;
@@ -15,9 +16,33 @@ public class PomarService {
 
     @Autowired
     private PomarRepository repository;
+    @Autowired
+    private ProdutorService produtorService;
 
     public Pomar findById(Integer id){
         Optional<Pomar> obj = repository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Pomar não encontrado! Id: "+id+", tipo: "+Pomar.class.getName()));
+    }
+
+    public List<Pomar> findAll(Integer idprod) {
+        produtorService.findById(idprod);
+        return repository.findAllByProdutor(idprod);
+    }
+
+    public Pomar update(Integer id, Pomar obj) {
+        Pomar newObj = findById(id);
+        updateData(newObj,obj);
+        return repository.save(newObj);
+    }
+
+    private void updateData(Pomar newObj, Pomar obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setLogradouro(obj.getLogradouro());
+        newObj.setBairro_localidade(obj.getBairro_localidade()); 
+        newObj.setCidade(obj.getCidade()); 
+        newObj.setEstado(obj.getEstado()); 
+        newObj.setCep(obj.getCep());
+        newObj.setProdutor(obj.getProdutor());
+        newObj.setRespTecnico(obj.getRespTecnico());
     }
 }
