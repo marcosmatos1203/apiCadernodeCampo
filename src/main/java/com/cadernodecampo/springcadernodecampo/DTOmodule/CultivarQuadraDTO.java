154c76222cc21bd8f5f3a1867ea5393e180cda1b
Matheus Lopes <matheusdefigueiredo@gmail.com>
2021-11-19 09:45:47 -0300

    Equals e hash para cultivar quadra dto

--- src/main/java/com/cadernodecampo/springcadernodecampo/DTOmodule/CultivarQuadraDTO.java
+++ src/main/java/com/cadernodecampo/springcadernodecampo/DTOmodule/CultivarQuadraDTO.java
@@ -29,4 +29,29 @@ public class CultivarQuadraDTO implements Serializable {
         this.quantidade = obj.quantidade;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CultivarQuadraDTO other = (CultivarQuadraDTO) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
